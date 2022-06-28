ad918bf372fbfe27f958672f828876277325bf3e
farahmzahra <farah20ti@mahasiswa.pcr.ac.id>
2022-06-28 12:56:54 +0700

    first push

--- application/views/dashboard/teknisi.php
+++ application/views/dashboard/teknisi.php
@@ -1,24 +1,11 @@
 <div class="container">
 	<div class="justify-content-md-center ">
 		<div class="text-center mt-5">
-			<img src="<?=base_url('assets/img/repair.png')?>" width="300px"/>
+			<img src="<?=base_url('assets/img/repair.png')?>" width="250px"/>
 		</div>
 		<div class="mt-4 text-center">
-			<h2>Selamat Bergabung di <b style="color:#CBD5B2">Go-Service</b>, Teknisi!</h2>
+			<h3>Selamat Bergabung di <b style="color:#CBD5B2">Go-Service</b>, Teknisi!</h3>
 			<a class="btn text-light mt-3" style="background-color:#CBD5B2" aria-current="page" href="#">Mari Bekerja</a>
-		</div>
-	</div>
-</div>
-<div class="mt-5 p-4" style="background:#536D5A">
-	<div class="row">
-		<div class="col-12 col-md-8">
-			<h2 style="color:#CBD5B2"><b>Go-Service!</b></h2>
-		</div>
-		<div class="col-6 col-md-2">
-			<h4 style="color:#FFFFFF"><b>Layanan</b></h4>
-		</div>
-		<div class="ol-6 col-md-2">
-			<h4 style="color:#FFFFFF"><b>Contact</b></h4>
 		</div>
 	</div>
 </div>
--- application/views/dashboard/user.php
+++ application/views/dashboard/user.php
@@ -7,34 +7,43 @@
 	</div>
 </div>
 <div class=" d-flex justify-content-center mt-5 p-5 " style="background:#CBD5B2">
-		<div class="row ">
-			<div class="col">
-				<div class="card mt-3 mx-5 p-3 justify-content-center">
-					<img src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
-					<a aria-current="page" href="#" class="text-center active fw-bold"
-						style="text-decoration:none;color:#536D5A">Servis<br>AC</a>
-				</div>
-			</div>
-            <div class="col">
-				<div class="card mt-3 mx-5 p-3 justify-content-center">
-					<img  class="center-block" src="<?=base_url('assets/img/elektronik.png')?>" width="100px" />
-					<a aria-current="page" href="#" class="text-center active fw-bold"
-						style="text-decoration:none;color:#536D5A">Servis Elektronik</a>
-				</div>
-			</div>
-            <div class="col">
-				<div class="card mt-3 mx-5 p-3 justify-content-center">
-					<img  class="center-block" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
-					<a aria-current="page" href="#" class="text-center active fw-bold"
-						style="text-decoration:none;color:#536D5A">Servis<br>Listrik</a>
-				</div>
-			</div>
-            <div class="col">
-				<div class="card mt-3 mx-5 p-3 justify-content-center">
-					<img  class="center-block" src="<?=base_url('assets/img/kran.png')?>" width="100px" />
-					<a aria-current="page" href="#" class="text-center active fw-bold"
-						style="text-decoration:none;color:#536D5A">Perbaikan Pipa</a>
-				</div>
+	<div class="row ">
+		<div class="col">
+			<div class="card mt-3 mx-5 p-3 justify-content-center">
+				<img src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+				<a aria-current="page" href="#" class="text-center active fw-bold"
+					style="text-decoration:none;color:#536D5A">Servis<br>AC</a>
 			</div>
 		</div>
+        <div class="col">
+			<div class="card mt-3 mx-5 p-3 justify-content-center">
+				<img  class="center-block" src="<?=base_url('assets/img/elektronik.png')?>" width="100px" />
+				<a aria-current="page" href="#" class="text-center active fw-bold"
+					style="text-decoration:none;color:#536D5A">Servis Elektronik</a>
+			</div>
+		</div>
+        <div class="col">
+			<div class="card mt-3 mx-5 p-3 justify-content-center">
+				<img  class="center-block" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+				<a aria-current="page" href="#" class="text-center active fw-bold"
+					style="text-decoration:none;color:#536D5A">Servis<br>Listrik</a>
+			</div>
+		</div>
+        <div class="col">
+			<div class="card mt-3 mx-5 p-3 justify-content-center">
+				<img  class="center-block" src="<?=base_url('assets/img/kran.png')?>" width="100px" />
+				<a aria-current="page" href="#" class="text-center active fw-bold"
+					style="text-decoration:none;color:#536D5A">Perbaikan Pipa</a>
+			</div>
+		</div>
+		
 	</div>
+</div>
+<div class="col-lg-12 text-center mt-5">
+	<h4 style="color:#536D5A"><b>Cara pesan dengan</b> <b style="color:#CBD5B2">Go-Service!</b></h4>
+</div>
+<div class="row">
+	<div class="col">
+		
+	</div>
+</div>
