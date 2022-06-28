40033f116e618d7247b0758cf96f269db4e782ec
farahmzahra <farah20ti@mahasiswa.pcr.ac.id>
2022-06-28 13:18:44 +0700

    second push

--- application/views/dashboard/user.php
+++ application/views/dashboard/user.php
@@ -1,37 +1,37 @@
 <div class="container">
 	<div class="row justify-content-md-center ">
 		<div class="col-lg-12 text-center mt-5">
-			<h2 style="color:#536D5A"><b>Go-Service! Siap Melayani Anda Dimanapun & Kapanpun</b></h2>
-			<h4 style="color:#CBD5B2"><b>Jasa Kami Menjangkau Wilayah Pekanbaru dan Sekitarnya</b></h4>
+			<h3 style="color:#536D5A"><b>Go-Service! Siap Melayani Anda Dimanapun & Kapanpun</b></h3>
+			<h5 style="color:#CBD5B2"><b>Jasa Kami Menjangkau Wilayah Pekanbaru dan Sekitarnya</b></h5>
 		</div>
 	</div>
 </div>
 <div class=" d-flex justify-content-center mt-5 p-5 " style="background:#CBD5B2">
 	<div class="row ">
 		<div class="col">
-			<div class="card mt-3 mx-5 p-3 justify-content-center">
-				<img src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+			<div class="card mt-3 mx-5 p-3 text-center">
+				<img class="ms-2" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
 				<a aria-current="page" href="#" class="text-center active fw-bold"
 					style="text-decoration:none;color:#536D5A">Servis<br>AC</a>
 			</div>
 		</div>
         <div class="col">
-			<div class="card mt-3 mx-5 p-3 justify-content-center">
-				<img  class="center-block" src="<?=base_url('assets/img/elektronik.png')?>" width="100px" />
+			<div class="card mt-3 mx-5 p-3 text-center">
+				<img class="ms-3" src="<?=base_url('assets/img/elektronik.png')?>" width="100px" />
 				<a aria-current="page" href="#" class="text-center active fw-bold"
 					style="text-decoration:none;color:#536D5A">Servis Elektronik</a>
 			</div>
 		</div>
         <div class="col">
-			<div class="card mt-3 mx-5 p-3 justify-content-center">
-				<img  class="center-block" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+			<div class="card mt-3 mx-5 p-3 text-center">
+				<img class="ms-2" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
 				<a aria-current="page" href="#" class="text-center active fw-bold"
 					style="text-decoration:none;color:#536D5A">Servis<br>Listrik</a>
 			</div>
 		</div>
         <div class="col">
-			<div class="card mt-3 mx-5 p-3 justify-content-center">
-				<img  class="center-block" src="<?=base_url('assets/img/kran.png')?>" width="100px" />
+			<div class="card mt-3 mx-5 p-3 text-center">
+				<img class="ms-2" src="<?=base_url('assets/img/kran.png')?>" width="100px" />
 				<a aria-current="page" href="#" class="text-center active fw-bold"
 					style="text-decoration:none;color:#536D5A">Perbaikan Pipa</a>
 			</div>
@@ -42,8 +42,29 @@
 <div class="col-lg-12 text-center mt-5">
 	<h4 style="color:#536D5A"><b>Cara pesan dengan</b> <b style="color:#CBD5B2">Go-Service!</b></h4>
 </div>
-<div class="row">
-	<div class="col">
-		
+<div class="justify-content-center mx-5 mt-3 p-auto">
+	<div class="row">
+		<div class="col">
+				<div class="mt-3 mx-5 p-3 text-center">
+					<img src="<?=base_url('assets/img/like.png')?>" width="70px"/><br>
+					<h5 aria-current="page" class="text-center active fw-bold"
+						style="text-decoration:none;">Pilih Layanan Jasa</h5>
+				</div>
+			</div>
+	        <div class="col">
+				<div class="mt-3 mx-5 p-3 text-center">
+					<img  class="center-block" src="<?=base_url('assets/img/time.png')?>" width="70px" /><br>
+					<h5 aria-current="page" class="text-center active fw-bold"
+						style="text-decoration:none;">Pilih Waktu & Teknisi</h5>
+				</div>
+			</div>
+	        <div class="col">
+				<div class="mt-3 mx-5 p-3 text-center">
+					<img  class="center-block" src="<?=base_url('assets/img/clean.png')?>" width="70px" /><br>
+					<h5 aria-current="page" class="text-center active fw-bold"
+						style="text-decoration:none;">Nikmati Layanan</h5>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
