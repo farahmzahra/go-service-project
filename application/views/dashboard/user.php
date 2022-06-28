57f428e32d4271057e2fb5d3ab0b517edd18de70
Farah Meutia Zahra <macbook@MACBOOKs-MacBook-Air.local>
2022-06-28 09:52:58 +0700

    Commit 1: Implementasi UI Halaman User dan Teknisi

---
+++ application/views/dashboard/user.php
@@ -0,0 +1,40 @@
+<div class="container">
+	<div class="row justify-content-md-center ">
+		<div class="col-lg-12 text-center mt-5">
+			<h2 style="color:#536D5A"><b>Go-Service! Siap Melayani Anda Dimanapun & Kapanpun</b></h2>
+			<h4 style="color:#CBD5B2"><b>Jasa Kami Menjangkau Wilayah Pekanbaru dan Sekitarnya</b></h4>
+		</div>
+	</div>
+</div>
+<div class=" d-flex justify-content-center mt-5 p-5 " style="background:#CBD5B2">
+		<div class="row ">
+			<div class="col">
+				<div class="card mt-3 mx-5 p-3 justify-content-center">
+					<img src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+					<a aria-current="page" href="#" class="text-center active fw-bold"
+						style="text-decoration:none;color:#536D5A">Servis<br>AC</a>
+				</div>
+			</div>
+            <div class="col">
+				<div class="card mt-3 mx-5 p-3 justify-content-center">
+					<img  class="center-block" src="<?=base_url('assets/img/elektronik.png')?>" width="100px" />
+					<a aria-current="page" href="#" class="text-center active fw-bold"
+						style="text-decoration:none;color:#536D5A">Servis Elektronik</a>
+				</div>
+			</div>
+            <div class="col">
+				<div class="card mt-3 mx-5 p-3 justify-content-center">
+					<img  class="center-block" src="<?=base_url('assets/img/ac_service.png')?>" width="100px" />
+					<a aria-current="page" href="#" class="text-center active fw-bold"
+						style="text-decoration:none;color:#536D5A">Servis<br>Listrik</a>
+				</div>
+			</div>
+            <div class="col">
+				<div class="card mt-3 mx-5 p-3 justify-content-center">
+					<img  class="center-block" src="<?=base_url('assets/img/kran.png')?>" width="100px" />
+					<a aria-current="page" href="#" class="text-center active fw-bold"
+						style="text-decoration:none;color:#536D5A">Perbaikan Pipa</a>
+				</div>
+			</div>
+		</div>
+	</div>
